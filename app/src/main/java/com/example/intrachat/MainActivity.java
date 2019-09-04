cb31e18a6a63d10fd8345d3d4e0650a74da1b7ca
chirayu-joshi <chirayujoshi1189@gmail.com>
2019-09-04 23:18:04 +0530

    temperarory sign in feature added

--- app/src/main/java/com/example/intrachat/MainActivity.java
+++ app/src/main/java/com/example/intrachat/MainActivity.java
@@ -1,18 +1,31 @@
 package com.example.intrachat;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     RelativeLayout rellay1, rellay2;
-
     Handler handler = new Handler();
-
     Runnable runnable = new Runnable() {
         @Override
         public void run() {
@@ -20,6 +33,14 @@ public class MainActivity extends AppCompatActivity {
             rellay2.setVisibility(View.VISIBLE);
         }
     };
+
+    private static final int RC_SIGN_IN = 9001;
+    private FirebaseAuth mAuth;
+    private GoogleSignInClient mGoogleSignInClient;
+    private ProgressDialog pDialog;
+    private TextView mStatusTextView;
+    private TextView mDetailTextView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,7 +48,71 @@ public class MainActivity extends AppCompatActivity {
 
         rellay1 = findViewById(R.id.rellay1);
         rellay2 = findViewById(R.id.rellay2);
-
         handler.postDelayed(runnable, 2000);
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.client_id))
+                .requestEmail()
+                .build();
+
+        findViewById(R.id.sign_in_button).setOnClickListener(this);
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        pDialog = new ProgressDialog(MainActivity.this);
+    }
+
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    private void signOut() {
+        mAuth.signOut();
+
+        mGoogleSignInClient.signOut().addOnCompleteListener(this,
+                new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        updateUI(null);
+                    }
+                });
+    }
+
+    private void updateUI(FirebaseUser user) {
+        hideProgressDialog();
+
+        TextView displayName = findViewById(R.id.displayName);
+        ImageView profileImage = findViewById(R.id.profilePic);
+        if (user != null) {
+            displayName.setText(user.getDisplayName());
+            displayName.setVisibility(View.VISIBLE);
+
+            profileImage.setVisibility(View.VISIBLE);
+            findViewById(R.id.sign_in_button).setVisibility(View.GONE);
+            findViewById(R.id.sign_out_button).setVisibility(View.VISIBLE);
+        } else {
+            mStatusTextView.setText(R.string.signed_out);
+            mDetailTextView.setText(null);
+
+            findViewById(R.id.sign_in_button).setVisibility(View.VISIBLE);
+            findViewById(R.id.sign_out_button).setVisibility(View.GONE);
+        }
+    }
+
+    private void hideProgressDialog() {
+            pDialog.dismiss();
+    }
+
+    @Override
+    public void onClick(View v) {
+        int i = v.getId();
+        if (i == R.id.sign_in_button) {
+            signIn();
+        } else if (i == R.id.sign_out_button) {
+            signOut();
+        }
     }
 }
